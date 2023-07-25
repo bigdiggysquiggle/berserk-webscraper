2eb928876f5323fda84e1ff83aae92c073942d0c
bigdiggysquiggle <randomcouchpotato@gmail.com>
2023-07-24 20:39:17 -0600

    trying to scrape punpun

---
+++ punpun.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+
+import os
+import shutil
+import img2pdf
+import requests
+import berserk_dl
+from bs4 import BeautifulSoup
+from PIL import Image
+
+base_url = "https://oyasumi-punpun-manga.com"
+
+def fix_imgs():
+	print("Bad images detected. Repaired automatically.")
+	def to_num(s):
+		return int(s.split('.')[0])
+	files = os.listdir()
+	files.sort(key=to_num)
+	for file in files:
+		with Image.open(file) as fixing:
+			print(file)
+			fixing.save("fixed " + file.split('.')[0] + ".jpeg")
+		os.remove(file)
+		os.rename("fixed " + file.split('.')[0] + ".jpeg", file)
+
+def dl_chap(chapter):
+	name = chapter.text
+	chapter=BeautifulSoup(requests.get(base_url + chapter.a['href']).text, 'html.parser')
+	pages=chapter.find_all('img')
+	if len(pages) == 1:
+		return
+	os.mkdir(name)
+	os.chdir(name)
+	i=1
+	for page in pages:
+		url=page.get('src')
+		if url[-1] != 'g':
+			url=url[:-1]
+		data=requests.get(url)
+		with open(str(i) + '.jpeg', 'wb') as file:
+			for chunk in data.iter_content(100000):
+				file.write(chunk)
+		i+=1
+
+def make_pdf(name):
+	def to_num(s):
+		return int(s.split('.')[0])
+	li=os.listdir()
+	li.sort(key=to_num)
+	imgs=[]
+	for fname in li:
+		if not fname.endswith(".jpeg"):
+			continue
+		path=os.getcwd() + "/" + fname
+		if os.path.isdir(path):
+			continue
+		imgs.append(path)
+	if not len(imgs):
+		return
+	file=name + ".pdf"
+	with open(file, "wb") as fi:
+		fi.write(img2pdf.convert(imgs))
+	shutil.move(file, os.getcwd().split("jpeg")[0] +  "pdf/" + name)
+
+if __name__ == '__main__':
+	berserk_dl.setup_dirs("Goodnight Punpun")
+	page=requests.get(base_url)
+	soup=BeautifulSoup(page.text, 'html.parser')
+	print("got site")
+	chapters=soup.select('#ceo_latest_comics_widget-3 > ul:nth-child(2)')[0].find_all('li')
+	for chapter in chapters:
+		chap = chapter.text
+		print(chap)
+		dl_chap(chapter)
+		try:
+			berserk_dl.make_pdf(chap, chap)
+		except:
+			fix_imgs()
+			berserk_dl.make_pdf(chap, chap)
